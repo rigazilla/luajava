c8ea0302c3c6e5b94442bde855fb4ace069999ac
rigazilla <vittorio@infinispan.org>
2025-10-01 11:44:38 +0200

    rollback part 5

--- lua51/jni/lua51/src/ldo.c
+++ lua51/jni/lua51/src/ldo.c
@@ -493,9 +493,9 @@ static void f_parser (lua_State *L, void *ud) {
   Proto *tf;
   Closure *cl;
   struct SParser *p = cast(struct SParser *, ud);
-  luaZ_lookahead(p->z);
+  int c = luaZ_lookahead(p->z);
   luaC_checkGC(L);
-  tf = (luaY_parser)(L, p->z,
+  tf = ((c == LUA_SIGNATURE[0]) ? luaU_undump : luaY_parser)(L, p->z,
                                                              &p->buff, p->name);
   cl = luaF_newLclosure(L, tf->nups, hvalue(gt(L)));
   cl->l.p = tf;
